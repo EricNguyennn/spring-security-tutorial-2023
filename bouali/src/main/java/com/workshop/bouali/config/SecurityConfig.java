f7db0395ac1edc3027972091d7bf2c82b75d1282
ziinHua <chhinhua@gmail.com>
2023-02-01 21:50:06 +0700

    implement userDetailService

--- bouali/src/main/java/com/workshop/bouali/config/SecurityConfig.java
+++ bouali/src/main/java/com/workshop/bouali/config/SecurityConfig.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.Filter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Chhin_Hua - 31/01
@@ -19,6 +26,19 @@ import javax.servlet.Filter;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+
+    private final static List<UserDetails> APPLICATION_USER = Arrays.asList(
+        new User(
+            "bouali.social@gmail.com",
+            "password",
+            Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        ),
+        new User(
+        "user.social@gmail.com",
+        "password",
+            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+        )
+    );
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,4 +52,12 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> APPLICATION_USER
+                .stream()
+                .filter(u -> u.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
+    }
 }
